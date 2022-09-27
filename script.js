b7d29fd0cdcbe2ce7c2975186c6f590166dc9714
SDann <smdann17@gmail.com>
2022-09-27 16:22:19 -0500

    In JavaScript: created a function for displaying the 5-day forecast data, which loops through the API data in increments of 8 to display a different day's data each time. Data displayed includes date, weather icon, temperature, humidity, and wind speed. Elements to hold data are created dynamically.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var APIKey = "66480ffae0c528a21ade847c422f34ac";
 const weatherDays = [];
 let currentDay = null;
+let todayDate = moment().format("M/DD/YYYY");
+
+let forecastDisplay = "";
 
 
 
@@ -8,7 +11,7 @@ let currentDay = null;
 // Makes the API call
 function getAPI(city) {
 
-  const queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+  const queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
 
   fetch(queryURL)
   
@@ -18,19 +21,22 @@ function getAPI(city) {
 
   .then(function(data) {
     console.log(data);
+    displayForecast(data.list)
     data.list.forEach(function(tsObj){
 
       // Makes a moment date object for each record
       const dateObj = moment.unix(tsObj.dt)
-
+      
       // Generates the day # for the day in the date object
       const dateNum = dateObj.format("DDD")
-
+      
       // If the current date in tsObj hasn't had a record put into weatherDays, do that now
       // Then skip over all other records for this day
-      if (dateNum !== currentDay && weatherDays.length < 5){
+      if (dateNum !== currentDay && weatherDays.length < 6){
         weatherDays.push(tsObj)
         currentDay = dateNum
+        
+        
       }
 
     })
@@ -38,11 +44,36 @@ function getAPI(city) {
     });
   }
 
+// Displays 5-day forecast (loop increments by 8 to exclude current day)
+function displayForecast(data){
+  
+  for (let i = 1; i < data.length; i += 8){
 
+    forecastDisplay += `
+    <div class="col-2">
+    <h5> ${data[i].dt_txt} </h5>
+      <div id="forecast-icon">
+      <img src="https://openweathermap.org/img/w/${data[i].weather[0].icon}.png">
+      </div>
+        <p> Temperature: ${data[i].main.temp} F </p>
+        <p> Humidity: ${data[i].main.humidity}% </p>
+        <p> Wind Speed: ${data[i].wind.speed} MPH </p>
+    </div>`
+  }
+  $(".forecast-row").append(forecastDisplay);
+}
+
+// Event listener on primary search button to initiate API GET with city name
 $("#primary").on("click", function citySearch(){
   console.log("button clicked");
   var citySearch = $("#enterCity").val();
   console.log(citySearch)
+
+  // Adds name of city searched and current date to heading of current weather section
+  var cityHeading = $("<h2>");
+  cityHeading.text(citySearch + " " + "(" + todayDate + ")")
+  $("#cityDate").append(cityHeading)
+
   getAPI(citySearch)
   
 })
@@ -50,7 +81,10 @@ $("#primary").on("click", function citySearch(){
 
   
 
-/* Remember that the query URL won't work automatically as it's written. You'll need to adjust your application to accept user input, to store in the city variable that you've created. */
+  
+  
+
+
 
 // I WANT to see the weather outlook for multiple cities
 
@@ -75,8 +109,7 @@ $("#primary").on("click", function citySearch(){
 // Uses local storage to store persistant data
 
 
-// Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. 
-// Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+
 
 
 /*
